e08c5ddea430ce9d59ec3119abf4e5fed63bd5d3
Josef Mastný <mastny.pepa@gmail.com>
2022-12-08 18:08:04 +0100

    Modální okno pomocí jQuery

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,13 @@
-var button = document.querySelector("#button");
-var modal = document.querySelector("#modal");
-var close = document.querySelector("#close");
+$(document).ready(function(){
+    var button = $("#button");
+    var modal = $("#modal");
+    var close = $("#close");
 
-button.addEventListener("click", function(){
-    modal.classList.add("modal_active");
-});
+    button.on("click", function(){
+        modal.addClass("modal_active");
+    });
 
-close.addEventListener("click", function(){
-    modal.classList.remove("modal_active");
+    close.on("click", function(){
+        modal.removeClass("modal_active");
+    });
 });
