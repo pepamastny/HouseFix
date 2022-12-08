aa7ce3ee459b579802e50a8596263a3c474c1d5b
Josef Mastný <mastny.pepa@gmail.com>
2022-12-08 19:21:45 +0100

    Nakódování sekce slidery, zprovoznění sliderů

--- src/js/main.js
+++ src/js/main.js
@@ -10,4 +10,26 @@ $(document).ready(function(){
     close.on("click", function(){
         modal.removeClass("modal_active");
     });
+
+    $(".slider").slick({
+        slidesToShow: 3,
+        prevArrow: $(".arrows__left"),
+        nextArrow: $(".arrows__right"),
+        responsive: [
+            {
+              breakpoint: 1200,
+              settings: {
+                slidesToShow: 2,
+              }
+            },
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 1,
+              }
+            },]
+    });
+
+    
+
 });
