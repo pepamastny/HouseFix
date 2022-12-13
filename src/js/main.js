7298e1ab2c9a44ee71e2903be18184fe8338d64b
Josef Mastný <mastny.pepa@gmail.com>
2022-12-13 19:04:04 +0100

    Animace projektu

--- src/js/main.js
+++ src/js/main.js
@@ -1,34 +1,39 @@
 $(document).ready(function(){
-    var button = $("#button");
-    var modal = $("#modal");
-    var close = $("#close");
 
-    button.on("click", function(){
-        modal.addClass("modal_active");
-    });
+  // Napojení knihovny wow.js
+  new WOW().init();
 
-    close.on("click", function(){
-        modal.removeClass("modal_active");
-    });
+  var button = $("#button");
+  var modal = $("#modal");
+  var close = $("#close");
 
-    $(".slider").slick({
-        slidesToShow: 3,
-        prevArrow: $(".arrows__left"),
-        nextArrow: $(".arrows__right"),
-        responsive: [
-            {
-              breakpoint: 1200,
-              settings: {
-                slidesToShow: 2,
-              }
-            },
-            {
-              breakpoint: 768,
-              settings: {
-                slidesToShow: 1,
-              }
-            },]
-    });
+  button.on("click", function(){
+    modal.addClass("modal_active");
+  });
+
+  close.on("click", function(){
+    modal.removeClass("modal_active");
+  });
+
+  // Napojení knihovny slideru
+  $(".slider").slick({
+    slidesToShow: 3,
+    prevArrow: $(".arrows__left"),
+    nextArrow: $(".arrows__right"),
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+        }
+      },]
+  });
 
     
 
